718730d3269f8ebd09677a014e76a9df17645939
Daniel <daniel.ruge-g@mail.escuelaing.edu.co>
2025-08-19 10:01:33 -0500

    clase que representa un hilo que imprimirá números entre un rango [A..B]

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -9,6 +9,33 @@ package edu.eci.arsw.threads;
  *
  * @author hcadavid
  */
-public class CountThread {
+public class CountThread extends Thread {
+
+    private int inicio;
+    private int fin;
+
+     /**
+     * Constructor que inicializa los límites del conteo.
+     * 
+     * @param inicio número inicial del rango
+     * @param fin número final del rango
+     */
+
+    public CountThread(int inicio, int fin){
+        this.inicio = inicio;
+        this.fin = fin;
+    }
+
+    /**
+     * Método run() que define lo que ejecuta el hilo.
+     * En este caso, imprime los números entre inicio y fin.
+     */
+
+    @Override
+    public void run(){
+        for(int i = inicio; i<=fin; i++){
+            System.out.println(i);
+        }
+    }
     
 }
